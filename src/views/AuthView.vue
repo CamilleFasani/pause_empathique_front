60714253039b178478a5dc00c68921e70cb21ee4
Pressense Fasani <camillefasani@gmail.com>
2026-06-12 14:28:43 +0200

    feat: update AuthView

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -4,21 +4,19 @@
     <button @click="router.push({ name: 'register' })">je n'ai pas de compte</button>
   </div>
   <LoginForm v-if="isLogin" />
-  <RegisterForm v-if="isRegister" />
+  <RegisterForm v-else />
 </template>
 <script lang="ts" setup>
 // useRoute pour lire la route actuelle
-import { useRoute } from 'vue-router'
 // useRouter pour naviguer vers une autre route
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+
 import { computed } from 'vue'
 import LoginForm from '../components/LoginForm.vue'
 import RegisterForm from '../components/RegisterForm.vue'
 
 const route = useRoute()
+const router = useRouter()
 
 const isLogin = computed(() => route.name === 'login')
-const isRegister = computed(() => route.name === 'register')
-
-const router = useRouter()
 </script>
