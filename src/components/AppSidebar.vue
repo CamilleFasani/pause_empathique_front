422b6ca6b2839314b6002b4e8c3f53be7386d327
Pressense Fasani <camillefasani@gmail.com>
2026-07-10 11:46:56 +0200

    feat: add desktop sidebar componenent

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -10,6 +10,26 @@
         <li>Ressources</li>
         <li>Contact</li>
       </ul>
+      <button
+        v-if="authStore.isAuthenticated"
+        type="button"
+        class="block w-full text-left border-t border-black/10 px-5 py-3 active:bg-bg-page"
+        @click="logout"
+      >
+        Se déconnecter
+      </button>
     </nav>
   </aside>
 </template>
+
+<script setup lang="ts">
+import { useAuthStore } from '../stores/auth'
+import router from '../router'
+
+const authStore = useAuthStore()
+
+const logout = async () => {
+  await authStore.logout()
+  await router.push({ name: 'welcome' })
+}
+</script>
